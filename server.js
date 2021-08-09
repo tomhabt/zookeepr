38656a80bd943d2fc04521604428fcc41711fd15
thomas.habtemariam <thomas.habtemariam@gmail.com>
2021-08-08 20:03:42 -0400

    query and params done

--- server.js
+++ server.js
@@ -1,38 +1,69 @@
-const { animals } = require('./data/animals');
 const express = require('express');
+const { animals } = require('./data/animals');
 
- 
-    // initialize the app
-const app = express();
 const PORT = process.env.PORT || 3001;
+const app = express();
 
-function filterByAnimals(animals, animalsArray) {
-    // let animalsArray = animalsArray;
-    if (animals.diet) {
-      animalsArray = animalsArray.filter(animal => animals.diet === animals.diet);
-    }
-    if (animals.species) {
-      animalsArray = animalsArray.filter(animal => animals.species === animals.species);
-    }
-    if (animals.name) {
-      animalsArray = animalsArray.filter(animal => animals.name === animals.name);
-    }
-    return animalsArray;
-  }
 
-  // request and response para
-app.get('/api/animals', (req, res) => {
-    // let results = animals;
-if (req.animals) {
-    animals = filterByAnimals(req.animals,animals);
+function filterByQuery(query, animalsArray) {
+    let personalityTraitsArray = [];
+    // Note that we save the animalsArray as filteredResults here:
+    let filteredResults = animalsArray;
+    if (query.personalityTraits) {
+      // Save personalityTraits as a dedicated array.
+      // If personalityTraits is a string, place it into a new array and save.
+      if (typeof query.personalityTraits === 'string') {
+        personalityTraitsArray = [query.personalityTraits];
+      } else {
+        personalityTraitsArray = query.personalityTraits;
+      }
+      // Loop through each trait in the personalityTraits array:
+      personalityTraitsArray.forEach(trait => {
+        // Check the trait against each animal in the filteredResults array.
+        // Remember, it is initially a copy of the animalsArray,
+        // but here we're updating it for each trait in the .forEach() loop.
+        // For each trait being targeted by the filter, the filteredResults
+        // array will then contain only the entries that contain the trait,
+        // so at the end we'll have an array of animals that have every one 
+        // of the traits when the .forEach() loop is finished.
+        filteredResults = filteredResults.filter(
+          animal => animal.personalityTraits.indexOf(trait) !== -1
+        );
+      });
+    }
+    if (query.diet) {
+      filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
+    }
+    if (query.species) {
+      filteredResults = filteredResults.filter(animal => animal.species === query.species);
+    }
+    if (query.name) {
+      filteredResults = filteredResults.filter(animal => animal.name === query.name);
+    }
+    // return the filtered results:
+    return filteredResults;
 }
-    res.json(animals);
-  });
 
+app.get('/api/animals',(req,res)=> {
+    let results = animals
+    if (req.query) {
+    results = filterByQuery (req.query, results)
+    }
+    res.json(results);
+});
+
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+  }
+  app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+      res.json(result);
+    } else {
+      res.send(404);
+    }
+  });
 app.listen(PORT, () => {
-    console.log(`API server now on port 3001!`);
-  });
-
-  
-
-  
+  console.log(`API server now on port ${PORT}!`);
+});
